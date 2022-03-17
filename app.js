c1959bf84c05b173b6114d0b45650667b8ce7397
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-17 11:13:02 -0700

    unfinished formEl event listener

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 // import functions and grab DOM elements
 
+const formEl = document.querySelector('form');
+const defeatedGoblinsEl = document.querySelector('.defeated-goblins');
+const goblinlistEl = document.querySelector('.goblin-list');
+const playerHPEl = document.querySelector('.player-hp');
+
 // let state
+let playerHP = 3;
+let goblinsDefeated = 0;
+
+const goblinArray [
+  { name: '', 
+    hp: 3 
+  }
+]
+
+// display goblins fxn 
+
 
 // set event listeners 
+formEl.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const data = new FormData(formEl);
+  //make a new goblin object with name and random HP
+  const newGoblin = {
+    name: data.get('goblin-name');
+    hp: Math.ceil(Math.random() * 3),
+  };
+
+  goblinArray.unshift(newGoblin);
+  // display goblin fxn 
+  formEl.reset();
+
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
