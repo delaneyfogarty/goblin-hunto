0c2e7ef7e8a2dff12b225ac45830fe872d91452c
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-18 12:54:31 -0700

    tryiing to fix event listener that broke

--- app.js
+++ app.js
@@ -42,51 +42,51 @@ formEl.addEventListener('submit', (event) => {
 });
 
 function displayGoblins() {
-  //clear out goblin list
+    //clear out goblin list
   goblinListEl.textContent = '';
 
-  //create an element for each goblin & append to goblinListEl
+    //create an element for each goblin & append to goblinListEl
   for (let i of goblinArray) {
     const goblinEl = renderGoblin(i);
 
+    if (i.hp > 0) {
+      goblinEl.addEventListener('click', () => {
 
-    goblinEl.addEventListener('click', () => {
+        if (playerHP < 0) {
+          alert('Game Over!');
+          return;
+        }
 
-      if (Math.random() > .5) {
-        i.hp--;
-        alert(`${i.name}` + 'got hit!');
-      } else {
-        alert('You missed!');
-      }
+        if (Math.random() < .5) {
+          playerHP--;
+          alert(i.name + ' : hit you!');
+        } else {
+          alert(i.name + ' : missed!');
+        }
 
-      if (Math.random() > .66) {
-        playerHP--;
-        alert(`${i.name}` + 'hit you!');
-      } else {
-        alert(`${i.name}` + 'missed!');
-      }
-      if (goblinEl.hp === 0) {
-        goblinsDefeated++;
-        defeatedGoblinsEl.textContent = `You have defeated ${goblinsDefeated} goblins`;
-      }
-      if (playerHPEl === 0) {
-        alert('game over');
-        return;
-      }
+        if (Math.random() < .33) {
+          i.hp--;
+          alert(i.name + ' : got hit');
+        } else {
+          alert('Goblin Hunter missed!');
+        }
 
+        if (i.hp === 0) {
+          goblinsDefeated++;
+        } else if (playerHP < 0) {
+          alert('Game Over!');
+        }
 
-      displayGoblins();
-      playerHPEl.textContent = `Your HP: ${playerHP}`;
+        playerHPEl.textContent = `Your HP: ${playerHP}`;
+        defeatedGoblinsEl.textContent = goblinsDefeated;
 
-    });
-
-    displayGoblins();
+        displayGoblins();
+      });
+    }
     goblinListEl.append(goblinEl);
-
   }
 }
-
-
+displayGoblins();
 
   // get user input
   // use user input to update state
